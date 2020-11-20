3ff8864bcf5efae52e7acb1ada57f29efc480ec3
Jason Onken <jason.onken@ibm.com>
2020-11-20 13:14:52 -0600

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=9352f66688d8501bb8571227d28e7a18&query=${latitude},${longitude}&units=f`
+
+  request({ url, json: true}, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined)
+    } else if (body.error) {
+      callback('Unable to find location', undefined)
+    } else {
+      callback(undefined, {
+        actualTemp: body.current.temperature,
+        feelsLike: body.current.feelslike,
+        condition: body.current.weather_descriptions[0]
+      })
+    }
+  })
+}
+
+module.exports = forecast
