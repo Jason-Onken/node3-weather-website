935202cdd23705ab1397270e9e10f4870689085d
Jason Onken <jason.onken@ibm.com>
2020-11-20 14:50:56 -0600

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3005
 
 // Define paths for Express configs
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -95,6 +96,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3005, () => {
-  console.log('Server is up on port 3005.')
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
 })
